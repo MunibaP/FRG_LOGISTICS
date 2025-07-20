7fb96656feb30e39c378bd7989f6a6d2023a24dc
MunibaP <munibapervez596@gmail.com>
2025-07-20 19:04:38 -0400

    changed the logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,49 +1,7 @@
-// import React from "react";
-// import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
-// import { Link } from "react-router-dom";
-// import "../styles/Header.css";
-
-// const Header = () => {
-//   return (
-//     <Navbar bg="light" expand="lg" sticky="top" className="shadow-sm">
-//       <Container>
-//         <Navbar.Brand as={Link} to="/" className="fw-bold text-success fs-4">
-//           FRG Logistics
-//         </Navbar.Brand>
-
-//         <Navbar.Toggle aria-controls="main-navbar" />
-//         <Navbar.Collapse id="main-navbar">
-//           <Nav className="ms-auto gap-2">
-//             <Nav.Link as={Link} to="/">Home</Nav.Link>
-//             <Nav.Link as={Link} to="/about">About Us</Nav.Link>
-
-//             <NavDropdown title="Services" id="services-dropdown">
-//               <NavDropdown.Item as={Link} to="/services/same-day">Same-Day Delivery</NavDropdown.Item>
-//               <NavDropdown.Item as={Link} to="/services/scheduled">Scheduled & Recurring</NavDropdown.Item>
-//               <NavDropdown.Item as={Link} to="/services/ecommerce">E-Commerce Parcel Delivery</NavDropdown.Item>
-//               <NavDropdown.Item as={Link} to="/services/retail">Retail & B2B Distribution</NavDropdown.Item>
-//               <NavDropdown.Item as={Link} to="/services/medical">Medical & Pharmacy Delivery</NavDropdown.Item>
-//               <NavDropdown.Item as={Link} to="/services/warehousing">Warehousing & Sorting</NavDropdown.Item>
-//             </NavDropdown>
-
-//             <Nav.Link as={Link} to="/fleet">Our Fleet</Nav.Link>
-//             <Nav.Link as={Link} to="/medical">Medical Delivery</Nav.Link>
-//             <Nav.Link as={Link} to="/get-quote">Get a Quote</Nav.Link>
-//             <Nav.Link as={Link} to="/track">Track</Nav.Link>
-//             <Nav.Link as={Link} to="/contact">Contact Us</Nav.Link>
-//           </Nav>
-//         </Navbar.Collapse>
-//       </Container>
-//     </Navbar>
-//   );
-// };
-
-// export default Header;
-
 import React from "react";
 import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
-import logo from "../assets/FRG.png";
+import logo from "../assets/logo1.png";
 import "../styles/Header.css"
 
 const Header = () => {
